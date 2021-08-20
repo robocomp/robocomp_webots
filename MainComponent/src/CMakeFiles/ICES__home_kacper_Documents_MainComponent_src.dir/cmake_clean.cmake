b2b72026ef5072f44b46e8d43081026203a7c7b2
Estox <kacp.kuj@gmail.com>
2021-08-20 15:10:58 +0200

    Remove cache files.

--- MainComponent/src/CMakeFiles/ICES__home_kacper_Documents_MainComponent_src.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ICES__home_kacper_Documents_MainComponent_src.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
